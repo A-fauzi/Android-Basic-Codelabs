f7ae7ce54c01fdf9127f64961792163476052128
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-30 20:58:23 +0700

    Add conditional behavior in Kotlin

--- unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_4/Main.kt
+++ unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_4/Main.kt
@@ -4,6 +4,27 @@ fun main() {
     val firstDice = Dice(6)
     println("Your ${firstDice.sides} side dice rolled ${firstDice.roll()}")
 
-    val secondDice = Dice(20)
-    println("Your ${secondDice.sides} side dice rolled ${secondDice.roll()}")
+    repeat(50) {
+        print("@")
+    }
+
+    println()
+
+    checkLuckyWin()
+
+}
+
+private fun checkLuckyWin() {
+    val dice = Dice(6)
+    val resultRoll = dice.roll()
+    val luckyNumber = 6
+
+    when(resultRoll) {
+        luckyNumber -> println("You Win!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        4 -> println("Don't cry! You rolled a 4. Try again!")
+        5 -> println("Apologies! You rolled a 5. Try again!")
+    }
 }
