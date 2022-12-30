a05d022103159f687c2f08a8dcae8b09a96bc49d
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-30 19:07:55 +0700

    Initial commit

---
+++ unit4connecttotheinternet/app/src/main/java/com/example/unit_4_connect_to_the_internet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unit_4_connect_to_the_internet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
