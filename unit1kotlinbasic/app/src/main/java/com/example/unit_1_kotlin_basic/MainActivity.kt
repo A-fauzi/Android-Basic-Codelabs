6360d957457d6da77337d4f385632c9cb2f0fcd3
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-30 20:10:03 +0700

    Basic Layouts

--- unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/MainActivity.kt
+++ unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.unit_1_kotlin_basic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.unit_1_kotlin_basic.pathway_2.BirthdayCardActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_path_3)
+        btn.setOnClickListener {
+            startActivity(Intent(this, BirthdayCardActivity::class.java))
+        }
     }
 }
---
+++ unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_2/BirthdayCardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.unit_1_kotlin_basic.pathway_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.unit_1_kotlin_basic.R
+
+class BirthdayCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_card)
+    }
+}
