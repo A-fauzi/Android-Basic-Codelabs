cf0c4ce96f025c3c48704310737f05e5178024db
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-30 22:02:42 +0700

    Dice Roller With Image

--- unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_4/DiceRollerActivity.kt
+++ unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_4/DiceRollerActivity.kt
@@ -1,10 +1,16 @@
 package com.example.unit_1_kotlin_basic.pathway_4
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import com.airbnb.lottie.LottieAnimationView
 import com.example.unit_1_kotlin_basic.R
+import kotlinx.coroutines.*
+import kotlin.coroutines.coroutineContext
 
 class DiceRollerActivity : AppCompatActivity() {
 
@@ -18,9 +24,46 @@ class DiceRollerActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     private fun rollDice() {
-        val resultRoll = findViewById<TextView>(R.id.tv_roll_number)
-        val dice = Dice(20)
-        resultRoll.text = dice.roll().toString()
+
+        var state: Boolean = false
+
+        val imageRoll = findViewById<ImageView>(R.id.iv_roll_number)
+        val animationView = findViewById<LottieAnimationView>(R.id.animationView)
+
+        val dice = Dice(6)
+
+        when (dice.roll()) {
+            1 -> {
+                imageRoll.setImageDrawable(resources.getDrawable(R.drawable.dice_1, null))
+                state = false
+            }
+            2 -> {
+                imageRoll.setImageDrawable(resources.getDrawable(R.drawable.dice_2, null))
+                state = false
+            }
+            3 -> {
+                imageRoll.setImageDrawable(resources.getDrawable(R.drawable.dice_3, null))
+                state = false
+            }
+            4 -> {
+                imageRoll.setImageDrawable(resources.getDrawable(R.drawable.dice_4, null))
+                state = false
+            }
+            5 -> {
+                imageRoll.setImageDrawable(resources.getDrawable(R.drawable.dice_5, null))
+                state = false
+            }
+            6 -> {
+                imageRoll.setImageDrawable(resources.getDrawable(R.drawable.dice_6, null))
+                state = true
+            }
+        }
+
+        when(state) {
+            true -> animationView.visibility = View.VISIBLE
+            false -> animationView.visibility = View.GONE
+        }
     }
 }
