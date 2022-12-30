ec88cff46c8454611dd8395029a0e7b3727bf6ec
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-30 20:47:51 +0700

    Interactive Dice Roller App

--- unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_4/DiceRollerActivity.kt
+++ unit1kotlinbasic/app/src/main/java/com/example/unit_1_kotlin_basic/pathway_4/DiceRollerActivity.kt
@@ -2,11 +2,25 @@ package com.example.unit_1_kotlin_basic.pathway_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.unit_1_kotlin_basic.R
 
 class DiceRollerActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_roller)
+
+        val btnRoll = findViewById<Button>(R.id.btn_roll)
+        btnRoll.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultRoll = findViewById<TextView>(R.id.tv_roll_number)
+        val dice = Dice(20)
+        resultRoll.text = dice.roll().toString()
     }
 }
