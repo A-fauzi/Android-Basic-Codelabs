2482158a6e938114ca54c406f59de003bc97aead
A-fauzi <akhmadfauzi0101@gmail.com>
2023-01-14 19:47:45 +0700

    Create XML layouts for Android

--- unit2layouts/app/src/main/java/com/example/unit_2_layouts/pathway_1/layout/MainActivity.kt
+++ unit2layouts/app/src/main/java/com/example/unit_2_layouts/pathway_1/layout/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.unit_2_layouts
+package com.example.unit_2_layouts.pathway_1.layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.unit_2_layouts.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
